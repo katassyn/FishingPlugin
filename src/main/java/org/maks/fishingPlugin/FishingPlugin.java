27f820405894b325e06762781ae9a4983fcf9d2d
katassyn <mastu2115@gmail.com>
2025-08-14 17:40:26 +0200

    1st

---
+++ src/main/java/org/maks/fishingPlugin/FishingPlugin.java
@@ -0,0 +1,17 @@
+package org.maks.fishingPlugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FishingPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
